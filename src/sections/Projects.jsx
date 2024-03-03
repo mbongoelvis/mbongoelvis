b60bc0430f7f0886dfb6f97e32247f986579ecb2
mbongoelvis <mbongombongo4@gmail.com>
2024-03-03 17:02:36 +0100

    first project added

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -8,16 +8,36 @@ export const Projects = () => {
           PROJECTS
         </h1>
         {/* first project */}
-        <div className="w-full min-h-[90rem] bg-red-400">
-          <a href="https://mbongo-elvis-country-api.vercel.app/"></a>
-          <div>
-            <img src="" alt="" />
-          </div>
-          <p>COUNTRY API</p>
-          <div>
-            <p>The Tools used as:</p>
-            <p>React, Tailwindcss git and country API from rapidAPI</p>
-            <p></p>
+        <div className="w-full grid grid-cols-2 min-h-screen gap-5">
+          <a
+            href="https://country-api-topaz-ten.vercel.app/" target="blank"
+            className="rounded overflow-hidden h-full hover:rounded-2xl group transition ease-in-out flex flex-col "
+          >
+            <div className="w-full h-full overflow-hidden">
+              <img
+                src="public/countryAPI.png"
+                alt=""
+                className="group-hover:scale-[1.1] transition ease-in-out object-cover"
+              />
+            </div>
+            <p className="mt-5 text-4xl font-bold opacity-[.8]">
+              Country Infomations
+            </p>
+            <div className="flex gap-3 mt-3">
+              <p className="text-xl font-bold text-gray-500 py-1 px-3 border rounded-full border-black">
+                REACT
+              </p>
+              <p className="text-xl font-bold text-gray-500 py-1 px-3 border rounded-full border-black">
+                TAILWINDCSS
+              </p>
+              <p className="text-xl font-bold text-gray-500 py-1 px-3 border rounded-full border-black">
+                COUNTRY API
+              </p>
+            </div>
+          </a>
+          <div className="w-full h-full grid gap-5 grid-rows-2">
+            <div className="bg-red-400 rounded-xl"></div>
+            <div className="bg-green-300 rounded-xl"></div>
           </div>
         </div>
       </div>
